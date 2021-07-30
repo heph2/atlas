75979fb237c6f6607f09a17c08cd2268c9c31b95
Heph <srht@mrkeebs.eu>
2021-07-31 00:30:22 +0200

    Initial Commit

---
+++ lib/App/atlas.pm
@@ -0,0 +1,23 @@
+package App::atlas;
+
+our $VERSION = 1.0;
+
+=head1 NAME
+
+App::Atlas - a simple feed rss generator
+
+=head1 DESCRIPTION
+
+This is a stub module, see F<script/atlas> for details of the app.
+
+=head1 AUTHOR
+
+Heph
+
+=head1 LICENSE
+
+GNU Affero General Public License
+
+=cut
+
+1;
